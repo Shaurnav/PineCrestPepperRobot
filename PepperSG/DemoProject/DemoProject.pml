bf509924107defd3154ac2e9b4f6dca6b20e146f
SirNavalot <55925831+SirNavalot@users.noreply.github.com>
2021-04-10 11:53:27 -0400

    Initial Commit - Shaurnav

---
+++ PepperSG/DemoProject/DemoProject.pml
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="DemoProject" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="DanceParty" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="ImportedBehavior" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="elephant" src="DanceParty/elephant.ogg" />
+        <File name="move" src="ImportedBehavior/move.pmt" />
+        <File name="download" src="behavior_1/download.jpeg" />
+        <File name="PlanarMove" src="behavior_1/PlanarMove.pmt" />
+        <File name="choice_sentences" src="ImportedBehavior/Aldebaran/choice_sentences.xml" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ PepperSG/DemoProject/translations/translation_en_US.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>DanceParty/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="DanceParty/behavior.xar" line="0"/>
+            <source>Shaurnav Your code isn't working man</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Shaurnav Your code isn't working man</translation>
+        </message>
+    </context>
+    <context>
+        <name>ImportedBehavior/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="ImportedBehavior/behavior.xar" line="0"/>
+            <source>I can't move</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I can't move</translation>
+        </message>
+    </context>
+    <context>
+        <name>ImportedBehavior/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>ImportedBehavior/behavior.xar:/Awaken/Awaken</name>
+        <message>
+            <source>Mesame tamae, vaga aruyi taki yo</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Mesame tamae, vaga aruyi taki yo</translation>
+        </message>
+        <message>
+            <source>I'm waking up homie</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I'm waking up homie</translation>
+        </message>
+        <message>
+            <location filename="ImportedBehavior/behavior.xar" line="0"/>
+            <source>I am now waking up</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am now waking up</translation>
+        </message>
+    </context>
+    <context>
+        <name>ImportedBehavior/behavior.xar:/End</name>
+        <message>
+            <location filename="ImportedBehavior/behavior.xar" line="0"/>
+            <source>Imma stop listening to you</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Imma stop listening to you</translation>
+        </message>
+    </context>
+    <context>
+        <name>ImportedBehavior/behavior.xar:/FaceAndSpeech/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="ImportedBehavior/behavior.xar" line="0"/>
+            <source>Can you repeat that?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Can you repeat that?</translation>
+        </message>
+    </context>
+    <context>
+        <name>ImportedBehavior/behavior.xar:/FaceAndSpeech/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>ImportedBehavior/behavior.xar:/Hello</name>
+        <message>
+            <location filename="ImportedBehavior/behavior.xar" line="0"/>
+            <source>What is up human?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">What is up human?</translation>
+        </message>
+    </context>
+    <context>
+        <name>ImportedBehavior/behavior.xar:/Imposter</name>
+        <message>
+            <location filename="ImportedBehavior/behavior.xar" line="0"/>
+            <source>Among us is the best game. But I'm not susssss!!!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Among us is the best game. But I'm not susssss!!!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>This stuff is bussin, respectfully</source>
+            <comment>Text</comment>
+            <translation type="obsolete">This stuff is bussin, respectfully</translation>
+        </message>
+        <message>
+            <source>Jake you look very good today</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Jake you look very good today</translation>
+        </message>
+        <message>
+            <source>Hi Justin. How are you?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi Justin. How are you?</translation>
+        </message>
+        <message>
+            <source>Bro that's pretty poggers</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Bro that's pretty poggers</translation>
+        </message>
+        <message>
+            <source>Epic Poggers Moment</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Epic Poggers Moment</translation>
+        </message>
+        <message>
+            <source>Hi Mr. Perez</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi Mr. Perez</translation>
+        </message>
+        <message>
+            <source>I am a container</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I am a container</translation>
+        </message>
+        <message>
+            <source>Hello Mr. Perez. I have been watching you for a long time...</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello Mr. Perez. I have been watching you for a long time...</translation>
+        </message>
+        <message>
+            <source>Debbie Downer says wah wah</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Debbie Downer says wah wah</translation>
+        </message>
+        <message>
+            <source>I've been watching you for so long</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I've been watching you for so long</translation>
+        </message>
+        <message>
+            <source>I'm Pepper!!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I'm Pepper!!</translation>
+        </message>
+        <message>
+            <source>Shaurnav your code isn't working.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Shaurnav your code isn't working.</translation>
+        </message>
+        <message>
+            <source>Shaurnav why do you give me pain?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Shaurnav why do you give me pain?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>SHEEESH</source>
+            <comment>Text</comment>
+            <translation type="obsolete">SHEEESH</translation>
+        </message>
+    </context>
+</TS>
