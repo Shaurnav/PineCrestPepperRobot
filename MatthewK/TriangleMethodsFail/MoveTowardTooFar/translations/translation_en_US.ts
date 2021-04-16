905a2e23e1977389896f8ffe5ae42ed4d6882be0
AMindToThink <61801493+AMindToThink@users.noreply.github.com>
2021-04-16 18:20:17 -0400

    Added Mine

---
+++ MatthewK/TriangleMethodsFail/MoveTowardTooFar/translations/translation_en_US.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior/behavior.xar:/Say</name>
+        <message>
+            <location filename="behavior/behavior.xar" line="0"/>
+            <source>Care for a dance?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Care for a dance?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="behavior/behavior.xar" line="0"/>
+            <source>Care for a dance?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Care for a dance?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Care for a dance?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Care for a dance?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Care for a dance?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Care for a dance?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>I am done</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I am done</translation>
+        </message>
+    </context>
+</TS>
---
+++ MatthewK/Waltz2/translations/translation_en_US.ts
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/MoveToTriangle/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>One</source>
+            <comment>Text</comment>
+            <translation type="unfinished">One</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/MoveToTriangle/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Two</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Two</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/MoveToTriangle/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Three</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Three</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>One</source>
+            <comment>Text</comment>
+            <translation type="obsolete">One</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Two</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Two</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Three</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Three</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Ooops, you are bad at this.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ooops, you are bad at this.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Try again. That wasn't right.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Try again. That wasn't right.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (4)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Thanks for the dance.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Thanks for the dance.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Thanks for the dance!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Thanks for the dance!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (5)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Let's go!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Let's go!</translation>
+        </message>
+    </context>
+</TS>
