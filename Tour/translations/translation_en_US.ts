e1e830f405b4cf8ce464f949b1d2a76bc645bd06
SirNavalot <55925831+SirNavalot@users.noreply.github.com>
2021-05-27 13:13:40 -0400

    push

--- Tour/translations/translation_en_US.ts
+++ Tour/translations/translation_en_US.ts
@@ -107,19 +107,17 @@
     <context>
         <name>behavior_1/behavior.xar:/FollowMe/FollowActions/Come Here Actions/Say</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hot stuff, comin' through!</source>
             <comment>Text</comment>
-            <translation type="unfinished">Hot stuff, comin' through!</translation>
+            <translation type="obsolete">Hot stuff, comin' through!</translation>
         </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/FollowMe/FollowActions/Come Here Actions/Say (2)</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>I'm here</source>
             <comment>Text</comment>
-            <translation type="unfinished">I'm here</translation>
+            <translation type="obsolete">I'm here</translation>
         </message>
     </context>
     <context>
